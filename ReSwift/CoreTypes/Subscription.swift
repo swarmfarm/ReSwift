01592d1b0996543b770f7b0a25fe49d8905f4787
Arie <hendriksearie@gmail.com>
2024-10-23 08:01:45 +1100

    Added some weak self to the subscription

--- ReSwift/CoreTypes/Subscription.swift
+++ ReSwift/CoreTypes/Subscription.swift
@@ -87,7 +87,7 @@ public class Subscription<State> {
         ) -> Subscription<Substate>
     {
         return Subscription<Substate> { sink in
-            self.observer = { oldState, newState in
+            self.observer = {  oldState, newState in
                 sink(oldState.map(selector) ?? nil, selector(newState))
             }
         }
@@ -100,8 +100,8 @@ public class Subscription<State> {
     public init(sink: @escaping (@escaping (State?, State) -> Void) -> Void) {
         // Provide the caller with a closure that will forward all values
         // to observers of this subscription.
-        sink { old, new in
-            self.newValues(oldState: old, newState: new)
+        sink { [weak self]  old, new in
+            self?.newValues(oldState: old, newState: new)
         }
     }
 
